3de9de29b00541d0bde286c5916926a007016822
James Rudrum <james_rudrum@outlook.com>
2019-04-24 20:08:23 +0100

    white_check_mark: Pass test for index having a list of posts with correct info

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
-# FactoryGirl.define do
-#   factory :post do
-#     caption "nofilter"
-#     image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/neon1.jpg', 'image/jpg')
-#   end
-# end
+FactoryGirl.define do
+  factory :post do
+    caption "nofilter"
+    image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/neon1.jpg', 'image/jpg')
+  end
+end
