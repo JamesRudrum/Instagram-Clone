17cd78730422f3efbbdd6ecd84ce2793684e0e7e
James Rudrum <james_rudrum@outlook.com>
2019-05-07 23:42:40 +0100

    :white_check_mark: Add and pass test to ensure user name is less than 12 chars

--- spec/features/create_new_user_spec.rb
+++ spec/features/create_new_user_spec.rb
@@ -27,8 +27,16 @@ feature 'Creating a new user' do
     fill_in 'Email', with: 'sxyrailsdev@myspace.com'
     fill_in 'Password', with: 'supersecret', match: :first
     fill_in 'Password confirmation', with: 'supersecret'
-
     click_button 'Sign up'
     expect(page).to have_content('minimum is 4 characters')
   end
+
+  scenario 'requires a user name to be less than 12 characters' do
+  fill_in 'User name', with: 'h' * 13
+  fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+  fill_in 'Password', with: 'supersecret', match: :first
+  fill_in 'Password confirmation', with: 'supersecret'
+  click_button 'Sign up'
+  expect(page).to have_content("maximum is 12 characters")
+end
 end
